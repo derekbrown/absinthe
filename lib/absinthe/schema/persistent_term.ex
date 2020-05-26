80b44fa61a32b7e2d6b6f6f461d915318e86732e
Derek Brown <derek@brown.lt>
2020-05-26 14:09:20 -0700

    Ensure PersistentTerm Loaded

--- lib/absinthe/schema/persistent_term.ex
+++ lib/absinthe/schema/persistent_term.ex
@@ -1,102 +1,104 @@
-if Code.ensure_loaded?(:persistent_term) do
-  defmodule Absinthe.Schema.PersistentTerm do
-    @moduledoc """
-    Experimental: Persistent Term based Schema Backend
 
-    By default, Absinthe schemas are stored in a generated module. If your schema
-    is called `MyAppWeb.Schema`, Absinthe creates a `MyAppWeb.Schema.Compiled`
-    module containing the structs and other data that Absinthe needs at runtime
-    to execute GraphQL operations against that schema.
+case Code.ensure_loaded(:persistent_term) do
+  {:module, _mod} ->
+    defmodule Absinthe.Schema.PersistentTerm do
+      @moduledoc """
+      Experimental: Persistent Term based Schema Backend
 
-    OTP introduced the `:persistent_term` module to provide many of the same
-    performance benefits of using compiled modules, without the downsides associated
-    with manipulating complex structures at compile time.
+      By default, Absinthe schemas are stored in a generated module. If your schema
+      is called `MyAppWeb.Schema`, Absinthe creates a `MyAppWeb.Schema.Compiled`
+      module containing the structs and other data that Absinthe needs at runtime
+      to execute GraphQL operations against that schema.
 
-    This module is an experimental effort at using the `:persistent_term` module
-    as an Absinthe schema backend. This module has been tested against against
-    the entire Absinthe test suite and shown to perform perhaps even better
-    than the compiled module.
+      OTP introduced the `:persistent_term` module to provide many of the same
+      performance benefits of using compiled modules, without the downsides associated
+      with manipulating complex structures at compile time.
 
-    To use:
+      This module is an experimental effort at using the `:persistent_term` module
+      as an Absinthe schema backend. This module has been tested against against
+      the entire Absinthe test suite and shown to perform perhaps even better
+      than the compiled module.
 
-    In your schema module:
-    ```
-    @schema_provider Absinthe.Schema.PersistentTerm
-    ```
+      To use:
 
-    In your application's supervision tree, prior to anywhere where you'd use
-    the schema:
-    ```
-    {Absinthe.Schema, MyAppWeb.Schema}
-    ```
+      In your schema module:
+      ```
+      @schema_provider Absinthe.Schema.PersistentTerm
+      ```
 
-    where MyAppWeb.Schema is the name of your schema.
-    """
+      In your application's supervision tree, prior to anywhere where you'd use
+      the schema:
+      ```
+      {Absinthe.Schema, MyAppWeb.Schema}
+      ```
 
-    @behaviour Absinthe.Schema.Provider
+      where MyAppWeb.Schema is the name of your schema.
+      """
 
-    def pipeline(pipeline) do
-      Enum.map(pipeline, fn
-        {Absinthe.Phase.Schema.Compile, options} ->
-          {Absinthe.Phase.Schema.PopulatePersistentTerm, options}
+      @behaviour Absinthe.Schema.Provider
 
-        phase ->
-          phase
-      end)
+      def pipeline(pipeline) do
+        Enum.map(pipeline, fn
+          {Absinthe.Phase.Schema.Compile, options} ->
+            {Absinthe.Phase.Schema.PopulatePersistentTerm, options}
+
+          phase ->
+            phase
+        end)
+      end
+
+      def __absinthe_type__(schema_mod, name) do
+        schema_mod
+        |> get()
+        |> Map.fetch!(:__absinthe_type__)
+        |> Map.get(name)
+      end
+
+      def __absinthe_directive__(schema_mod, name) do
+        schema_mod
+        |> get()
+        |> Map.fetch!(:__absinthe_directive__)
+        |> Map.get(name)
+      end
+
+      def __absinthe_types__(schema_mod) do
+        schema_mod
+        |> get()
+        |> Map.fetch!(:__absinthe_types__)
+        |> Map.fetch!(:referenced)
+      end
+
+      def __absinthe_types__(schema_mod, group) do
+        schema_mod
+        |> get()
+        |> Map.fetch!(:__absinthe_types__)
+        |> Map.fetch!(group)
+      end
+
+      def __absinthe_directives__(schema_mod) do
+        schema_mod
+        |> get()
+        |> Map.fetch!(:__absinthe_directives__)
+      end
+
+      def __absinthe_interface_implementors__(schema_mod) do
+        schema_mod
+        |> get()
+        |> Map.fetch!(:__absinthe_interface_implementors__)
+      end
+
+      def __absinthe_prototype_schema__(schema_mod) do
+        schema_mod
+        |> get()
+        |> Map.fetch!(:__absinthe_prototype_schema__)
+      end
+
+      @dialyzer {:nowarn_function, [get: 1]}
+      defp get(schema) do
+        :persistent_term.get({__MODULE__, schema})
+      end
     end
-
-    def __absinthe_type__(schema_mod, name) do
-      schema_mod
-      |> get()
-      |> Map.fetch!(:__absinthe_type__)
-      |> Map.get(name)
-    end
-
-    def __absinthe_directive__(schema_mod, name) do
-      schema_mod
-      |> get()
-      |> Map.fetch!(:__absinthe_directive__)
-      |> Map.get(name)
-    end
-
-    def __absinthe_types__(schema_mod) do
-      schema_mod
-      |> get()
-      |> Map.fetch!(:__absinthe_types__)
-      |> Map.fetch!(:referenced)
-    end
-
-    def __absinthe_types__(schema_mod, group) do
-      schema_mod
-      |> get()
-      |> Map.fetch!(:__absinthe_types__)
-      |> Map.fetch!(group)
-    end
-
-    def __absinthe_directives__(schema_mod) do
-      schema_mod
-      |> get()
-      |> Map.fetch!(:__absinthe_directives__)
-    end
-
-    def __absinthe_interface_implementors__(schema_mod) do
-      schema_mod
-      |> get()
-      |> Map.fetch!(:__absinthe_interface_implementors__)
-    end
-
-    def __absinthe_prototype_schema__(schema_mod) do
-      schema_mod
-      |> get()
-      |> Map.fetch!(:__absinthe_prototype_schema__)
-    end
-
-    @dialyzer {:nowarn_function, [get: 1]}
-    defp get(schema) do
-      :persistent_term.get({__MODULE__, schema})
-    end
-  end
-else
+{:error, _err} ->
   defmodule Absinthe.Schema.PersistentTerm do
     @moduledoc false
 
